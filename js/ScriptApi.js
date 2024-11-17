e7e31804b63ad477fa9e31ac0f97f4632a3c157c
Md Sojib Ali <alisojib295@gmail.com>
2024-11-17 09:34:30 +0600

    2nd

--- js/ScriptApi.js
+++ js/ScriptApi.js
@@ -3,7 +3,7 @@ const quoteEl = document.querySelector("#quote");
 const authorEl = document.querySelector("#author");
 const buttonEl = document.querySelector("#button");
 
-const urlAPI = "https://api.api-ninjas.com/v1/inflation?country=Canada";
+const urlAPI = "https://api.api-ninjas.com/v1/quotes?category=beauty";
 const keyAPI = "hyx6EYaUk5nZaiAaY4uBnw==met18E5Plh6BMs6j";
 
 const getQoute = () => {
@@ -12,10 +12,13 @@ const getQoute = () => {
   })
     .then((data) => data.json())
     .then((item) => {
-      quoteEl.textContent = item[0].country;
-      authorEl.textContent = item[0].period;
+      quoteEl.textContent = item[0].quote;
+      authorEl.textContent = item[0].author;
 
       console.log(item);
+    })
+    .catch((error) => {
+      console.log("Error Fatching Quotes", error);
     });
 };
 getQoute();
